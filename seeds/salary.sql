8852a4a3de404b4f301741165d73c2927d306f6c
Mark Chuang <markchuangth@gmail.com>
2022-05-13 13:56:54 -0700

    commit

--- seeds/salary.sql
+++ seeds/salary.sql
@@ -4,4 +4,5 @@ INSERT INTO salary (name, salary) VALUES ('John', 50000);
 INSERT INTO salary (name, salary) VALUES ('Mark', 60000);
 INSERT INTO salary (name, salary) VALUES ('Ben', 45000);
 INSERT INTO salary (name, salary) VALUES ('Mary', 90000);
-INSERT INTO slate (name, slate) VALUES ("Melissa", 1100000)
+INSERT INTO salary (name, slate) VALUES ("Melissa", 1100000);
+INSERT INTO salary (name, slate) VALUES ("Chris", 30000);
